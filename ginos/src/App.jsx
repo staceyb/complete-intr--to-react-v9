9e6833ad98444500039af72e0f1aff5d0e22c9bf
S <stacey.blaschke@gmail.com>
2024-11-01 16:30:48 -0700

    Add strict mode around all of App

--- ginos/src/App.jsx
+++ ginos/src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
 import Order from "./Order";
-
 
 const App = () => {
   return (
+    <StrictMode>
     <div>
       <h1 className="logo">Padre Gino's</h1>
       <Order />
     </div>
+    </StrictMode>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
