45ba76e13121ca1103f32b2caa1a807716375a70
S <stacey.blaschke@gmail.com>
2024-11-01 16:55:37 -0700

    Added PizzaOfTheDay component which calls api/pizza-of-the-day and uses a custom hook. Also added a debugvalue to easily identify the custom hook

--- ginos/src/App.jsx
+++ ginos/src/App.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <h1 className="logo">Padre Gino's</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
     </StrictMode>
   );
