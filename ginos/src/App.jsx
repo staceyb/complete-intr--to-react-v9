af714a6543b7438271262df8907ca87d7bc518e9
S <stacey.blaschke@gmail.com>
2024-11-01 15:34:06 -0700

    Add Order component to add a pizza choosing type and size. using useState Hook

--- ginos/src/App.jsx
+++ ginos/src/App.jsx
@@ -1,27 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
+import Order from "./Order";
 
 
 const App = () => {
   return (
     <div>
       <h1 className="logo">Padre Gino's</h1>
-      <Pizza 
-        name="The Pepperoni Pizza"
-        description="Mozzarella Cheese, Pepperoni"
-        image={"/public/pizzas/pepperoni.webp"} 
-      />
-      <Pizza 
-        name="The Hawaiian Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza 
-        name="The Big Meat Pizza"
-        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
     </div>
   );
 };
