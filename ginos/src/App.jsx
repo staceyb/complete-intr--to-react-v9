609fd0aadd108bfdf9a64377ccd955b1965e30eb
S <stacey.blaschke@gmail.com>
2024-11-01 17:56:46 -0700

    Add a header to the page that includes a cart icon with the number of items in the cart displayed (pseudo number for now) will replace with logic with correct items number next

--- ginos/src/App.jsx
+++ ginos/src/App.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
-import PizzaOfTheDay from "./PizzaOfTheDay";
+import PizzaOfTheDay from "./PizzaofTheDay";
+import Header from "./Header";
 
 const App = () => {
   return (
     <StrictMode>
     <div>
-      <h1 className="logo">Padre Gino's</h1>
+      <Header />
       <Order />
       <PizzaOfTheDay />
     </div>
