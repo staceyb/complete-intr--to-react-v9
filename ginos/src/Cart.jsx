86159fbb7ff78563a5a8c30f141b37f46e0478d9
S <stacey.blaschke@gmail.com>
2024-12-13 19:21:02 -0800

    Formatting changes to Cart component

--- ginos/src/Cart.jsx
+++ ginos/src/Cart.jsx
@@ -13,16 +13,16 @@ export default function Cart({ cart, checkout }) {
     <div className="cart">
       <h2>Cart</h2>
       <ul>
-        {cart.map((item, index) => (
-          <li key={index}>
-            <span className="size">{item.size}</span> –
-            <span className="type">{item.pizza.name}</span> –
-            <span className="price">{item.price}</span>
+        { cart.map((item, index) => (
+          <li key={ index }>
+            <span className="size">{ item.size }</span> –
+            <span className="type">{ item.pizza.name }</span> –
+            <span className="price">{ item.price }</span>
           </li>
-        ))}
+        )) }
       </ul>
-      <p>Total: {intl.format(total)}</p>
-      <button onClick={checkout}>Checkout</button>
+      <p>Total: { intl.format(total) }</p>
+      <button onClick={ checkout }>Checkout</button>
     </div>
   );
 }
