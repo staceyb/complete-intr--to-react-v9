592faa005d19eb3955ef324bce1305c0fe5f5603
S <stacey.blaschke@gmail.com>
2024-12-16 00:03:12 -0800

    Added comments to eslint config

--- ginos/eslint.config.mjs
+++ ginos/eslint.config.mjs
@@ -2,11 +2,12 @@ import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
-import pluginQuery from "@tanstack/eslint-plugin-query"
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
+/* .all is another option in place of .recoommended but can be overkill */,
 export default [
-  js.configs.recommended,
+  js.configs.recommended, 
   {
     ...reactPlugin.configs.flat.recommended,
     settings: {
@@ -20,10 +21,13 @@ export default [
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaFeatures: {
-          jsx: true,
+          jsx: true /* add in jsx */,
         },
       },
     },
@@ -32,5 +36,8 @@ export default [
       "react/prop-types": "off",
     },
   },
+  /* prettier always has to be last as it only turn off formatting, it does
+    not add anything, prevents eslint from warning about formatting things that 
+    do not matter as prettier will take care of it */
   prettier,
 ];
